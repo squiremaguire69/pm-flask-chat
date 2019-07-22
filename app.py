d4169e7cfd44cbc43a63232c4b60875bf2daf54f
Ubuntu <ubuntu@ip-172-31-33-144.ec2.internal>
2019-07-22 11:15:48 +0000

    Tidy up code and hide secret key

--- app.py
+++ app.py
@@ -1,31 +1,43 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
+
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET", "randomstring123")
 messages = []
 
-def add_messages(username, message):
-    """Add messages to the messages list"""
-    messages.append("{}: {}".format(username, message))
-    
-def get_all_messages():
-    """Get all of the messages and separate with a br"""
-    return"<br>".join(messages)
 
-@app.route('/')
+def add_message(username, message):
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append({"timestamp": now, "from": username, "message": message})
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
-    
-@app.route('/<username>')
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("index.html")
+
+
+@app.route("/chat/<username>", methods=["GET", "POST"])
 def user(username):
-    """Display chat messages"""
-    return "Welcome, {0} - {1}".format(username, get_all_messages())
-    
-@app.route('/<username>/<message>')
-def send_message(username, message):
-    """Create a new message and redirect to the chat page"""
-    add_messages(username, message)
-    return redirect("/" + username)
-    
-app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 5000)), debug=True )
+    """Add and display chat messages"""
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
+
+
+app.run(host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", "5000")), debug=False)
